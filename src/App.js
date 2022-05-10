a3be8a456258a65b2517f894be8248f719719ff4
manstarg <sam@Samuels-MacBook-Pro.local>
2022-05-10 10:14:27 +0100

    updated to lesson 7

--- src/App.js
+++ src/App.js
@@ -1,66 +1,43 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
-import CardList from './components/card-list/card-list.component';
-import SearchBox from './components/search-box/search-box-component';
-
-
 const App = () => {
-
-  const [monsters, setMonsters] = useState([])
-  const [ searchValue, setSearchValue] = useState('')
-  const[filteredMonsters, setFilteredMonsters]=useState(monsters)
-  
-
-  useEffect(() => {
-
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response) => response.json())
-  .then(users => setMonsters(users))
-  },[])
-
-  useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchValue)
-    })
-    setFilteredMonsters(newFilteredMonsters)
-   
-  },[monsters,searchValue])
-
-
-
-const onSearchChange = (event) => {
-  const searchValues = event.target.value.toLowerCase()
- 
-  setSearchValue(searchValues)
-
-}
-
-
-
-
-
-    
-  return(
-    <div className="App">
-    <h1 className='app-title'>Manster Monsters!</h1>
-
- <SearchBox 
- className='monsters-search-box' 
- onChangeHandler={onSearchChange} 
- placeholder='search monsters'
-
- />
-
-
-
-
-<CardList monsters={filteredMonsters}/>
-
-</div>
-  )
-}
-
-
+  return (
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
